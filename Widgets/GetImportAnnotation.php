00eae4467b1256d402d960fcca47b76cdafe6c3e
ChardonneauR <contact@ronan-chardonneau.fr>
2023-09-15 15:24:37 +0200

    we fixed lots of bugs, we clean the code

--- Widgets/GetImportAnnotation.php
+++ Widgets/GetImportAnnotation.php
@@ -7,7 +7,7 @@ use Piwik\Widget\WidgetConfig;
 
 class GetImportAnnotation extends Widget
 {
-        public static function configure(WidgetConfig $config)
+    public static function configure(WidgetConfig $config)
     {
         $config->setCategoryId('Annotations_Annotations');
         $config->setName('ImportAnnotationWidget_ImportAnnotation');
@@ -15,27 +15,8 @@ class GetImportAnnotation extends Widget
     }
 
 
-public function render()
-{
-    return $this->renderTemplate('widget');
-}
-    private function isValidDate($date)
+    public function render()
     {
-        return preg_match('/^\d{4}-\d{2}-\d{2}$/', $date);
-    }
-
-    private function constructAnnotationUrl($date, $note)
-    {
-        return "method=Annotations.add&idSite=1&token_auth={$this->token}&date=" . urlencode($date) . "&note=" . urlencode($note);
-    }
-
-    private function constructBulkUrl($urls)
-    {
-        $encodedUrls = [];
-        foreach ($urls as $url) {
-            $encodedUrls[] = "urls[]=" . urlencode($url);
-        }
-
-        return "{$this->baseUrl}/index.php?module=API&method=API.getBulkRequest&format=json&" . implode('&', $encodedUrls);
+        return $this->renderTemplate('widget');
     }
 }
